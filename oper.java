ea1d29551ab992a70736389244862ebee48222ec
Pawanmaurya56 <pawankmaurya2518@gmail.com>
2025-07-30 17:58:28 +0530

    Add files via upload

---
+++ oper.java
@@ -0,0 +1,28 @@
+package TEST;
+
+import java.util.Scanner;
+
+class test extends oper{
+	void add(int a,int d) {
+		int b,c;
+		b=20;
+		c=a+b;
+		System.out.println(c);
+	}
+}
+
+public class oper {
+	void add(int a,int b) {
+		Scanner ab=new 
+		int c;
+		c=a+b;
+		System.out.println(c);
+						
+	}
+	public static void main(String[] args) {
+		oper ad=new oper();
+		ad.add(12, 12);
+		
+	}
+
+}
